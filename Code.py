468359517944acf2ca5d225a582f881107d0b9f5
Станислав <stanislav@192.168.1.5>
2022-11-17 18:21:38 +0300

    final!

---
+++ Code.py
@@ -0,0 +1,33 @@
+empty_l = []
+ empty_l.append(empty_l)
+
+print(empty_l)
+print(empty_l.copy())
+
+Class penis_her:
+Class
+Class:
+def  something
+def Something
+def kek_l8ol
+def kek_l8ol(something)
+def kek_l8ol(something):
+def kek_l8ol   (something):
+def
+def kek_Lol
+def long_def_test_case:
+def long_test case_with multiple_words
+def :
+class rRemovesuffix:
+class Removesuffix
+class Removesuffix:
+class Remov8esuffix
+class Remov8esuffix:
+class Remov8_esuffix:
+class Remov_8esuffix:
+class Removesu4ffixC sdfsdf:
+class RemovesuffixC sdfsdf(:
+class RemovesuffixC sdfsdf():
+class      RemoveTest     Sdfsdf:
+class      RemoveTest     Sdfsdf(line):
+class RemovesuffixC(PenisHer):
